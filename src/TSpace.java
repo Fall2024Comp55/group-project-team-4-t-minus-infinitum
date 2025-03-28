7170f37cdefda6af0e05e3850c0e61564aaee0f9
Truc Nhat Minh Bui <t_bui22@u.pacific.edu>
2025-03-28 15:52:29 -0700

    Finished implementing TSpace class.

--- src/TSpace.java
+++ src/TSpace.java
@@ -1,4 +1,40 @@
 
 public class TSpace {
-
+	private int Row;
+	private int Column;
+	
+	public TSpace(int row, int col)
+	{
+		this.Row = row;
+		this.Column = col;
+	}
+	
+	//Declare getters and setters
+	
+	public int getRow()
+	{
+		return Row;
+	}
+	
+	public int getColumn()
+	{
+		return Column;
+	}
+	
+	public void setRow(int row)
+	{
+		this.Row = row;
+	}
+	
+	public void setColumn(int col)
+	{
+		this.Column = col;
+	}
+	
+	@Override
+	public String toString() {
+	    return "(" + Row + "," + Column + ")";
+	}
+	
+	
 }
