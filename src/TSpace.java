3f2cdda0e218ece1cabbac93ce63231fc9eacfa0
Truc Nhat Minh Bui <t_bui22@u.pacific.edu>
2025-03-30 13:21:42 -0700

    Finished implementing the TSpace class.

--- src/TSpace.java
+++ src/TSpace.java
@@ -36,4 +36,13 @@ public class TSpace {
 	public String toString() {
 	    return "(" + Row + "," + Column + ")";
 	}
+	
+	public static void main(String[] args) {
+		TSpace one = new TSpace(3, 4);
+		TSpace two = new TSpace(1, 6);
+		two.setRow(two.getRow()+1);
+		two.setColumn(two.getColumn()-1);
+		System.out.println("one r: " + one.getRow() + ", c: " + one.getColumn());
+		System.out.println("two r: " + two.getRow() + ", c: " + two.getColumn());
+		}
 }
