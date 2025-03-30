c6584fb646951cd23d54431688f54b55aa6ce576
Truc Nhat Minh Bui <t_bui22@u.pacific.edu>
2025-03-30 14:09:40 -0700

    1st implementation of the UserSpaceship class.

--- src/UserSpaceship.java
+++ src/UserSpaceship.java
@@ -1,4 +1,13 @@
 
 public class UserSpaceship {
-
+	private int startRow;
+	private int startColumn;
+	private SpaceshipType sType;
+	
+	public UserSpaceship(SpaceshipType Spaceship, int startRow, int startCol)
+	{
+		this.startRow = startRow;
+		this.startColumn = startCol;
+		this.sType = Spaceship;
+	}
 }
