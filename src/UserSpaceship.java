42d236df19b7a7c1a7d6fdf20a8ee7004205f7c5
Truc Nhat Minh Bui <t_bui22@u.pacific.edu>
2025-04-02 16:48:25 -0700

    Added a println for UserSpaceship class.

--- src/UserSpaceship.java
+++ src/UserSpaceship.java
@@ -43,8 +43,8 @@ public class UserSpaceship {
 	{
 		this.sType = Spaceship;
 	}
-	
-	// prints out more legibly the row & columns for an array of spaces
+
+	//prints out more legibly the row & columns for an array of spaces
 	public static void printSpaces(TSpace[] arr) {
 		 for(int i = 0; i < arr.length; i++) {
 		System.out.print("r" + arr[i].getRow() + "c" + arr[i].getColumn() + "; ");
