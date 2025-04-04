899cbd36c49fac0510daa4bc210f7db618c00325
Truc Nhat Minh Bui <t_bui22@u.pacific.edu>
2025-04-04 16:34:04 -0700

    2nd change for the Enemyship1 class.

--- src/Enemyship1.java
+++ src/Enemyship1.java
@@ -1,8 +1,12 @@
+import acm.graphics.*;
+import java.awt.*;
 
-public class Enemyship1 {
+public class Enemyship1{
 	private int startRow;
 	private int startColumn;
 	private SpaceshipType sType;
+	
+	 public static final int SIZE = 40;
 	
 	public Enemyship1(SpaceshipType Enemyship1, int startRow, int startCol)
 	{
@@ -42,5 +46,25 @@ public class Enemyship1 {
 	public void setSpaceshipType (SpaceshipType Enemyship1)
 	{
 		this.sType = Enemyship1;
+	}
+	
+	public GPolygon getVisual() {
+	    int x = startColumn * SIZE;
+	    int y = startRow * SIZE;
+
+	    GPolygon triangle = new GPolygon();
+
+	    // Add vertices relative to center
+	    triangle.addVertex(0, -SIZE / 2);        // Top point
+	    triangle.addVertex(SIZE / 2, SIZE / 2);  // Bottom right
+	    triangle.addVertex(-SIZE / 2, SIZE / 2); // Bottom left
+
+	    triangle.setFilled(true);
+	    triangle.setColor(Color.RED);
+
+	    // Move the triangle to (x, y)
+	    triangle.setLocation(x, y);
+
+	    return triangle;
 	}
 }
