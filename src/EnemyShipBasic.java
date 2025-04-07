70645490f704508529aaf8a15f90589bad559ba6
Talvir <t_singh13@u.pacific.edu>
2025-04-07 15:56:13 -0700

    Changes to enum

--- src/EnemyShipBasic.java
+++ src/EnemyShipBasic.java
@@ -57,7 +57,7 @@ public class EnemyShipBasic{
 	    // Triangle pointing downward:
 	    triangle.addVertex(0, SIZE / 2);          // Bottom center
 	    triangle.addVertex(SIZE / 1, -SIZE / 2);   // Top right
-	    triangle.addVertex(-SIZE / 1, -SIZE / 2);  // Top left
+	    triangle.addVertex(-SIZE / 1, -SIZE / 1);  // Top left
 
 	    triangle.setFilled(true);
 	    triangle.setLocation(x, y);
